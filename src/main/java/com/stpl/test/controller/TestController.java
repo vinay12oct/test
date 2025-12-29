08336646075e685e2493ef3a1be50bcc806da0fe
vinay12oct <vinaypatel.siet@gmail.com>
2025-12-29 16:27:28 +0530

    initial commit

---
+++ src/main/java/com/stpl/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.stpl.test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+
+
+    @GetMapping("/get")
+    public String getResult(){
+        return "Hello world";
+    }
+}
+
+
